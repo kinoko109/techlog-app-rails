69cc6c90a66b694ca75bc1df903db1b26253f3e9
kinoko109 <tatsuaki1009@gmail.com>
2025-04-15 19:24:14 +0900

    test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { 'テスト太郎' }
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    password { '123456' }
+    password_confirmation { '123456' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:nickname) { 'テスト太郎' }
+  let(:email) { 'test@example.com' }
+
+  describe '.first' do
+    before do
+      # factoryを省略してcreateメソッドのみで可能
+      create(:user, nickname: nickname, email: email)
+    end
+
+    subject { described_class.first }
+
+    it '事前に作成した通りのUserを返す' do
+      expect(subject.nickname).to eq('テスト太郎')
+      expect(subject.email).to eq('test@example.com')
+    end
+  end
 end
